3eb70b09b2d082fad1f40059b6afedb19fc6f434
muhmadjaradat <muhmadjradat@gmail.com>
2021-03-29 18:29:18 +0300

    adding lab12

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 'use strict';
-let leftIndex;
-let rightIndex;
-let middleIndex;
+let views=[];
+let votes=[];
 const names = [
   'bag',
   'banana',
@@ -56,79 +55,158 @@ for(let i =0;i<names.length;i++){
 }
 console.log(Image.all);
 
-let numberOne = 0;
-let numberTwo = 0;
-let numberThree = 0;
-
-
+let r ;
+let r1;
+let arr = [3,2,4];
+let arr1 = [2];
 function randomNumber(min, max){
-  numberOne = Math.floor(Math.random() * (max - min + 1)) + min;
-  do {
-    numberTwo = Math.floor(Math.random() * (max - min + 1)) + min;
-  } while(numberOne === numberTwo);
 
+  arr =[];
 
-  do {
-    numberThree = Math.floor(Math.random() * (max - min + 1)) + min;
-  } while(numberThree === numberTwo || numberThree === numberOne);
+  console.log(arr.some((val) => arr1.indexOf(val) !== -1));
+  while(arr.length < 3){
+
+    do {
+      r = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    } while (arr1.includes(r));
+    if(arr.indexOf(r) === -1) arr.push(r);}
+  arr1 =[];
+  while(arr1.length < 3 ){
+    do {
+      r1 = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    } while ((arr.includes(r1)));
+    if(arr1.indexOf(r1) === -1) arr1.push(r1);
+
+  }
+  console.log(arr);
+  console.log(arr1);
+
 
 }
+randomNumber(0,Image.all.length-1);
 
+let count=0;
 function render(){
-  randomNumber(0,Image.all.length-1);
-
-  leftImage.src = Image.all[numberOne].path;
-  leftImage.alt = Image.all[numberOne].name;
-  leftImage.title = Image.all[numberOne].name;
-  console.log(numberOne);
 
 
-  rightImage.src = Image.all[numberTwo].path;
-  rightImage.alt = Image.all[numberTwo].name;
-  rightImage.title = Image.all[numberTwo].name;
-  console.log(numberTwo);
+  if (count%2 ===0) {
+
+    leftImage.src = Image.all[arr[0]].path;
+    leftImage.alt = Image.all[arr[0]].name;
+    leftImage.title = Image.all[arr[0]].name;
+
+    rightImage.src = Image.all[arr[1]].path;
+    rightImage.alt = Image.all[arr[1]].name;
+    rightImage.title = Image.all[arr[1]].name;
+
+    middleImage.src = Image.all[arr[2]].path;
+    middleImage.alt = Image.all[arr[2]].name;
+    middleImage.title = Image.all[arr[2]].name;
+
+    Image.all[arr[0]].views++;
+    Image.all[arr[1]].views++;
+    Image.all[arr[2]].views++;}
+  else {
+    leftImage.src = Image.all[arr1[0]].path;
+    leftImage.alt = Image.all[arr1[0]].name;
+    leftImage.title = Image.all[arr1[0]].name;
+
+    rightImage.src = Image.all[arr1[1]].path;
+    rightImage.alt = Image.all[arr1[1]].name;
+    rightImage.title = Image.all[arr1[1]].name;
 
 
-  middleImage.src = Image.all[numberThree].path;
-  middleImage.alt = Image.all[numberThree].name;
-  middleImage.title = Image.all[numberThree].name;
-  console.log(numberThree);
 
-  Image.all[numberOne].views++;
-  Image.all[numberTwo].views++;
-  Image.all[numberThree].views++;
+    middleImage.src = Image.all[arr1[2]].path;
+    middleImage.alt = Image.all[arr1[2]].name;
+    middleImage.title = Image.all[arr1[2]].name;
+
+    Image.all[arr1[0]].views++;
+    Image.all[arr1[1]].views++;
+    Image.all[arr1[2]].views++;
+    randomNumber(0,Image.all.length-1);}
 }
 
 imagesSection.addEventListener('click',handelClick);
 let rounds=25;
-let count=0;
+
 function handelClick(event){
 
   if(event.target.id !== 'imageSection'){
-    if(event.target.id === rightImage.id)
-    {
-      Image.all[numberTwo].votes++;
+    if (count%2===0) {
+      if(event.target.id === rightImage.id)
+      {
+        Image.all[arr[1]].votes++;
+      }
+      else if (event.target.id === leftImage.id){
+        Image.all[arr[0]].votes++;
+      }
+      else {
+        Image.all[arr[2]].votes++;
+      }
+      count+=1;
+      render();
     }
-    else if (event.target.id === leftImage.id){
-      Image.all[numberOne].votes++;
-    }
+
     else {
-      Image.all[numberThree].votes++;
-    }
-    count+=1;}
+      if(event.target.id === rightImage.id)
+      {
+        Image.all[arr1[1]].votes++;
+      }
+      else if (event.target.id === leftImage.id){
+        Image.all[arr1[0]].votes++;
+      }
+      else {
+        Image.all[arr1[2]].votes++;
+      }
+      count+=1;
+      render();
+    }}
   if (count===rounds){
     imagesSection.removeEventListener('click',handelClick);
     const results=buttonSec.appendChild(document.createElement('button'));
     results.textContent='Click to see results';
     results.addEventListener('click',addResults);
+
   }
-  render();
-
 }
-
 function addResults(event){
   h3El.textContent='Results';
+
   for (let i = 0; i < Image.all.length; i++) {
-    const liEl=resultsList.appendChild(document.createElement('li'));
-    liEl.textContent=`${Image.all[i].name} has ${Image.all[i].votes} votes. and was seen ${Image.all[i].views} times.`;}}
+    votes.push(Image.all[i].votes);
+    views.push(Image.all[i].views);
+  }
+  chartRender();
+}
 render();
+
+function chartRender() {
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+      labels: names,
+      datasets: [{
+        label: 'Product votes',
+        backgroundColor: 'red',
+        borderColor: 'rgb(255, 99, 132)',
+        data: votes
+      },
+      {
+        label: 'Product views',
+        backgroundColor: 'green',
+        borderColor: 'rgb(255, 99, 132)',
+        data: views
+      }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+}
